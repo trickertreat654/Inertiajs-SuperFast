a486135428bd066a7c38ec72195d04ed2e05a31a
Travis Ricker <trickertreat654@gmail.com>
2025-01-16 23:23:37 -0800

    fixed faker errer

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,8 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' =>'bobo',
+            'email' => 'bobo@cobo.com',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
